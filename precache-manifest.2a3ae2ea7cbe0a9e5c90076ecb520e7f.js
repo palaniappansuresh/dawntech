716f9dd9e3e2ddb415086105af4c7c285fb6acc6
Palaniappan S <dawntechdevelopers@gmail.com>
2020-07-25 12:18:02 +0530

    Updates

---
+++ precache-manifest.2a3ae2ea7cbe0a9e5c90076ecb520e7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e3dcb07e43d4b1b5825397a2947d60",
+    "url": "/dawntech/index.html"
+  },
+  {
+    "revision": "2a72ef1dc40c55acec57",
+    "url": "/dawntech/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "e22fddec1b3b8d297b42",
+    "url": "/dawntech/static/css/main.525e160c.chunk.css"
+  },
+  {
+    "revision": "2a72ef1dc40c55acec57",
+    "url": "/dawntech/static/js/2.aa652b88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dawntech/static/js/2.aa652b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22fddec1b3b8d297b42",
+    "url": "/dawntech/static/js/main.ca6e7dee.chunk.js"
+  },
+  {
+    "revision": "ca9228edee582aedfd36",
+    "url": "/dawntech/static/js/runtime-main.4ad31bd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dawntech/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dawntech/precache-manifest.c1f979ea073364d4134043b6ec1bdb8a.js"
+  "/dawntech/precache-manifest.2a3ae2ea7cbe0a9e5c90076ecb520e7f.js"
 );
 
 self.addEventListener('message', (event) => {
