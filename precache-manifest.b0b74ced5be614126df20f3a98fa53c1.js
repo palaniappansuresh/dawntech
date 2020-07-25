9ecc1e108eea88ac272e5437f6e99fa1033689e4
Palaniappan S <dawntechdevelopers@gmail.com>
2020-07-25 10:14:50 +0530

    Updates

---
+++ precache-manifest.b0b74ced5be614126df20f3a98fa53c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f7b120a960791916394cd0e163aaff",
+    "url": "/dawntech/index.html"
+  },
+  {
+    "revision": "604606dc70c97600b64e",
+    "url": "/dawntech/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/dawntech/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dawntech/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604606dc70c97600b64e",
+    "url": "/dawntech/static/js/main.27df3392.chunk.js"
+  },
+  {
+    "revision": "ca9228edee582aedfd36",
+    "url": "/dawntech/static/js/runtime-main.4ad31bd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dawntech/static/media/logo.5d5d9eef.svg"
+  }
+]);
